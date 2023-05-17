776fbb5421ead0b5694159ec29aa3dbf2a10ecb7
amscherer <88399829+amscherer@users.noreply.github.com>
2023-05-16 20:23:49 -0400

    final site changes

--- script.js
+++ script.js
@@ -1,36 +1,5 @@
-let timeline = d3.select('#timeline')
-
-timeline.append('rect')
-    .attr("x",620)
-    .attr("y",50)
-    .attr("width", 5 )
-    .attr("height", 600 )
-    .attr("fill",'white')
-    .attr("border-radius", "10px");
-
-timeline.append('circle')
-    .attr("cx",624)
-    .attr("cy",70)
-    .attr("r",10)
-    .attr("fill", 'grey')
-    .attr("stroke", 'white');
-
-timeline.append('circle')
-    .attr("cx",624)
-    .attr("cy",70)
-    .attr("r",10)
-    .attr("fill", 'grey')
-    .attr("stroke", 'white');
-
-timeline.append('circle')
-    .attr("cx",624)
-    .attr("cy",70)
-    .attr("r",10)
-    .attr("fill", 'grey')
-    .attr("stroke", 'white');
-
 let ntasAPI = 'https://data.cityofnewyork.us/resource/q2z5-ai38.geojson';
-// let ntasAPI = 'https://data.cityofnewyork.us/resource/9nt8-h7nd.geojson';
+
 
 // Initialize selected year as blank
 let selectedYear = '';
@@ -99,20 +68,6 @@ let drawMap = () => {
             }
             
         })
-       
-    
-    
-        // .on('mouseover', (ntaDataItem) =>{
-        //     tooltip.transition()
-        //         .style('visibility', 'visible')
-
-        //     let id = ntaDataItem['id']
-        //     let nta = violationData.find((item) => {
-        //         return item.ntaname === ntaname
-        //     })
-            
-        //     tooltip.text(nta['ntaname'] + ' - ' + )
-        // })
 };
 
 // Function to async load the violation data for a specific year 
@@ -174,60 +129,4 @@ d3.selectAll('.year-btn')
         loadViolationData(selectedYear);
         
     });
-    
-// set the dimensions and margins of the graph
-const margin = {top: 30, right: 30, bottom: 150, left: 60},
-    width = 1300 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-const svg = d3.select("#smallMultiples1")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-// Parse the Data
-d3.json('aggregatedData/agdata2018.json').then ( function(data) {
-
-  // sort data
-  data.sort(function(b, a) {
-    return a.bblviolations - b.bblviolations;
-  });
-
-  const top10 = data.slice(0,10);
-
-  // X axis
-  const x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(top10.map(d => d.ntaname))
-    .padding(0.2);
-  svg.append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(x))
-    .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
-
-  // Add Y axis
-  const y = d3.scaleLinear()
-    .domain([0, d3.max(top10, d => d.bblviolations)])
-    .range([ height, 0]);
-  svg.append("g")
-    .call(d3.axisLeft(y));
-
-  // Bars
-  svg.selectAll("mybar")
-    .data(top10)
-    .enter()
-    .append("rect")
-      .attr("x", d => x(d.ntaname))
-      .attr("y", d => y(d.bblviolations))
-      .attr("width", x.bandwidth())
-      .attr("height", d => height - y(d.bblviolations))
-      .attr("fill", "#999999")
-      .attr("font-size", '20px')
-
-});
 
