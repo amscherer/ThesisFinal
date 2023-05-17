3c1d225276d6c32c1ac9e5d378a3ff060bfe97e3
amscherer <88399829+amscherer@users.noreply.github.com>
2023-05-16 21:06:52 -0400

    installed more

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,10 @@ const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
+const jshint = require('gulp-jshint');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const terser = require('gulp-terser');
 
 // Expose the sas to css task
 function sassworkflow() {
@@ -31,6 +35,33 @@ function sassworkflow() {
 // $ gulp sassworkflow
 exports.sassworkflow = sassworkflow;
 
+function lint() {
+    return gulp
+        .src('src/js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+};
+exports.lint = lint;
+
+// Concatenate & Minify JS
+function scripts() {
+    return gulp
+        .src([
+            'node_modules/jquery/dist/jquery.js',
+            './src/js/*.js'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('scripts.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(terser())
+        .pipe(rename('scripts.min.js'))
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(browserSync.stream());
+};
+exports.scripts = scripts;
+
+
 function watchtask(){
     browserSync.init({
         server: {
@@ -40,12 +71,13 @@ function watchtask(){
     });
 
     gulp.watch( './src/sass/**/*.scss', sassworkflow );
+    gulp.watch( './src/js/*.js', gulp.parallel( scripts, lint) );
 }
 
 // Expose the task by exporting it
 exports.watchtask = watchtask;
 
 // build the parallal task
-const build = gulp.parallel(sassworkflow);
+const build = gulp.parallel(sassworkflow, scripts);
 
 gulp.task('default', gulp.series(build, watchtask));
